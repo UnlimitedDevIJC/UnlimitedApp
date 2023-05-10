d5586433f4094dcee3cc0bad9170998126298f25
GoncaloTavaresIJC <goncalo.tavares@isctejuniorconsulting.com>
2023-05-10 20:00:23 +0100

    Login

--- App.js
+++ App.js
@@ -1,10 +1,9 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
 
 export default function App() {
   return (
-    <View>
-      <Text></Text>
-    </View>
-  );
+    <NavigationContainer>
+      <MainStack />
+    </NavigationContainer>
+  )
 }
