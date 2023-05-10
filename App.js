0a8ccfebca6ebb3f53e27b588af01015b59a3eab
GoncaloTavaresIJC <goncalo.tavares@isctejuniorconsulting.com>
2023-05-10 20:03:49 +0100

    Update App.js

--- App.js
+++ App.js
@@ -1,9 +1,21 @@
+import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+import { firebase, initializeApp } from "firebase/app";
+import firebaseConfig from './Config/firebase';
+import MainStack from './Navigators/mainStack';
+
+
+const database = initializeApp(firebaseConfig)
 
 export default function App() {
-  return (
+ 
+  
+    return (
+      
     <NavigationContainer>
-      <MainStack />
-    </NavigationContainer>
-  )
+         <MainStack />
+      </NavigationContainer>
+      
+  );
+  
 }
