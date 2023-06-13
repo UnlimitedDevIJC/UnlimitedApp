528e84c1aed797c4cf0807f60097c2aaebe8cd96
GoncaloTavaresIJC <goncalo.tavares@isctejuniorconsulting.com>
2023-06-13 21:51:27 +0100

    agenda

--- UnlimitedApp/Paginas/Agenda/Agenda.js
+++ UnlimitedApp/Paginas/Agenda/Agenda.js
@@ -1,21 +1,62 @@
 import {
-    View,
-    Text,
-    TextInput,
-    SafeAreaView,
-    ScrollView,
-    TouchableOpacity,
-    KeyboardAvoidingView,
-    Platform,
-    TouchableWithoutFeedback,
-    Keyboard,
-    Image,
-    Alert,
-    ImageBackground,
-  } from "react-native"
+  View,
+  Text,
+  TextInput,
+  SafeAreaView,
+  ScrollView,
+  TouchableOpacity,
+  KeyboardAvoidingView,
+  Platform,
+  TouchableWithoutFeedback,
+  Keyboard,
+  Image,
+  Alert,
+  ImageBackground,
+} from "react-native"
+import {
+  getFirestore,
+  collection,
+  onSnapshot,
+  addDoc,
+  deleteDocs,
+  getDoc,
+  doc,
+  QuerySnapshot,
+} from "firebase/firestore"
 import styles from "./AgendaStyle"
+import { FontAwesome5, FontAwesome } from "@expo/vector-icons"
+import { useState } from "react"
+import SelectDropdown from "react-native-select-dropdown"
+
+const db = getFirestore()
+const colRef = collection(db, "Evento")
+
+let listaEventos = []
+let listaEventosFiltrada = []
+let filtros = []
+
+onSnapshot(colRef, (snapshot) => {
+  let mounted = true
+  if (mounted) {
+    listaEventos = []
+    snapshot.docs.forEach((doc) => {
+      listaEventos.push({ ...doc.data(), id: doc.id })
+    })
+    listaEventos.sort((a, b) => {
+      return a.inicio - b.inicio
+    })
+    // listaEventosFiltrada = listaEventos.filter((item) => {
+    //   return item.hora_inicio.toDate().getDate() == 2
+    // })
+    filtros = [...new Set(listaEventos.map((item) => item.palavrasChave))]
+    filtros.unshift("Todos") //Vai buscar os valores das areas sem estarem repetidos e acrescenta "Todos" ao inicio
+  }
+  return () => (mounted = false)
+})
 
 const Agenda = () => {
+  const [filtro, setFiltro] = useState("")
+
   return (
     <SafeAreaView style={styles.safeArea}>
       <ScrollView style={styles.scrollView} bounces={false}>
@@ -33,6 +74,45 @@ const Agenda = () => {
               style={styles.logo}
               source={require("../Login/unlimitedLogo.png")}
             />
+
+            {/* Barra de Pesquisa e filtro / palavras-chave*/}
+            <View style={styles.searchView}>
+              <TextInput
+                placeholder="Pesquisa..."
+                type="text"
+                onChangeText={(text) => alterarFiltro(text)}
+                value={filtro}
+                style={styles.searchInput}
+              />
+              {/* <TouchableOpacity>
+                <FontAwesome5 name={"filter"} style={styles.filterIcon} />
+              </TouchableOpacity> */}
+              <SelectDropdown
+                data={filtros}
+                buttonTextAfterSelection={(selectedItem, index) => {
+                  return selectedItem
+                }}
+                rowTextForSelection={(item, index) => {
+                  return item
+                }}
+                renderDropdownIcon={(isOpened) => {
+                  return (
+                    <FontAwesome5 name={"filter"} color={"#444"} size={18} style={styles.filterIcon}/>
+                  )
+                }}
+                defaultValue={"Todos"}
+                defaultButtonText="Todos"
+                dropdownIconPosition="left"
+                rowTextStyle={{ fontWeight: "600" }}
+                buttonStyle={{
+                  borderRadius: 15,
+                  width: "100%",
+                  height: 40,
+                  backgroundColor: "transparent",
+                }}
+                dropdownStyle={{ borderRadius: 15, width: "30%" }}
+              />
+            </View>
           </>
         </TouchableWithoutFeedback>
       </ScrollView>
--- UnlimitedApp/Paginas/Agenda/AgendaStyle.js
+++ UnlimitedApp/Paginas/Agenda/AgendaStyle.js
@@ -3,16 +3,16 @@ import { Platform, StyleSheet } from "react-native"
 const styles = StyleSheet.create({
   safeArea: {
     flex: 1,
-    backgroundColor: "#16508D",
+    backgroundColor: "#1A649F",
   },
 
   scrollView: {
     height: "100%",
-    backgroundColor:'white'
+    backgroundColor: "#F2F3F5",
   },
 
   retanguloFundo: {
-    backgroundColor: "#16508D",
+    backgroundColor: "#1A649F",
     width: "110%",
     height: 130,
     transform: [{ rotateZ: "-15deg" }],
@@ -23,9 +23,39 @@ const styles = StyleSheet.create({
   logo: {
     width: 85,
     height: 85,
-    alignSelf:'center',
-    position:'absolute',
-    top:'16%'
+    alignSelf: "center",
+    position: "absolute",
+    top: "16%",
+  },
+
+  searchView: {
+    width: "100%",
+    height: 40,
+    flexDirection: "row",
+  },
+
+  searchInput: {
+    width: "70%",
+    height: "100%",
+    fontSize: 14,
+    marginLeft: 20,
+    padding: 10,
+    backgroundColor: "white",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 6,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 20,
+    borderRadius: 5
+  },
+
+  filterIcon: {
+    marginLeft: 30,
+    fontSize: 35,
+    color: '#174162'
   },
 })
 
