488ce7cef7c5c4db5058bf8d87ec488d032bad64
GoncaloTavaresIJC <goncalo.tavares@isctejuniorconsulting.com>
2023-07-06 00:19:29 +0100

    Update EditarPerfil.js

--- UnlimitedApp/Paginas/EditarPerfil/EditarPerfil.js
+++ UnlimitedApp/Paginas/EditarPerfil/EditarPerfil.js
@@ -62,86 +62,6 @@ const EditarPerfil = ({ navigation }) => {
     }
   }, [])
 
-  const storage = getStorage()
-  const storageRef = ref(storage, "imagens/" + utilizador.email)
-
-  const [image, setImage] = useState(null)
-  const [uploading, setUploading] = useState(null)
-  const [image2, setImage2] = useState(null)
-
-  // const getImage = async () => {
-  //   getDownloadURL(ref(storage, "imagens/" + utilizador.email))
-  //     .then((url) => {
-  //       // `url` is the download URL for 'images/stars.jpg'
-  //       setImage2(url)
-  //       console.log(url)
-  //       // This can be downloaded directly:
-  //       const xhr = new XMLHttpRequest()
-  //       xhr.responseType = "blob"
-  //       xhr.onload = (event) => {
-  //         const blob = xhr.response
-  //       }
-  //       xhr.open("GET", url)
-  //       xhr.send()
-
-  //       // Or inserted into an <img> element
-  //       const img = document.getElementById("myimg")
-  //       img.setAttribute("src", url)
-  //     })
-  //     .catch((error) => {
-  //       // Handle any errors
-  //     })
-  // }
-
-  const pickImage = async () => {
-    try {
-      let result = await ImagePicker.launchImageLibraryAsync({
-        mediaTypes: ImagePicker.MediaTypeOptions.All,
-        allowsEditing: true,
-        aspect: [3, 3],
-        quality: 1,
-      })
-
-      console.log("Aqui")
-      console.log(result)
-      // const selectedAsset = result.assets[0]
-
-      const source = {
-        uri: result.uri,
-        width: result.width,
-        height: result.height,
-        type: result.type,
-        fileSize: result.fileSize,
-      }
-
-      console.log(source)
-      setImage(source)
-      console.log(storageRef)
-    } catch (error) {
-      console.log("Error occurred during image selection:", error)
-    }
-  }
-
-  const uploadImage = async () => {
-    setUploading(true)
-    const response = await fetch(image.uri)
-    const blob = await response.blob()
-    var ref = uploadBytes(storageRef, blob).then((snapshot) => {
-      console.log("Uploaded a blob or file!")
-    })
-
-    try {
-      await ref
-    } catch (e) {
-      console.log(e)
-    }
-
-    setUploading(false)
-
-    Alert.alert("Photo Uploaded!")
-    setImage(null)
-  }
-
   const handleUpdate = async () => {
     utilizadorRef = doc(db, "Utilizador", user.email)
     updateDoc(utilizadorRef, {
@@ -155,8 +75,6 @@ const EditarPerfil = ({ navigation }) => {
     }).then(() => {
       navigation.navigate("Perfil")
     })
-
-    uploadImage()
   }
 
   return (
@@ -178,13 +96,8 @@ const EditarPerfil = ({ navigation }) => {
             />
 
             <View style={styles.perfilContainer}>
-              <TouchableOpacity style={styles.editFotoBtn} onPress={pickImage}>
-                {image && (
-                  <Image
-                    style={styles.perfilImage}
-                    source={{ uri: image.uri }}
-                  />
-                )}
+              <TouchableOpacity style={styles.editFotoBtn}>
+                <Image style={styles.perfilImage} source={require("../Perfil/foto.jpeg")} />
                 <View style={styles.editFoto} />
                 <FontAwesome5 name="pen" style={styles.editFotoIcon} />
               </TouchableOpacity>
