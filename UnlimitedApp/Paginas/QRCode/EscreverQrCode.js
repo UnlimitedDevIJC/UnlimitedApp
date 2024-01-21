83e04c929dbe786606ce8989d784753ddba6fab2
GoncaloTavaresIJC <129522910+GoncaloTavaresIJC@users.noreply.github.com>
2024-01-21 13:54:54 +0000

    update

--- UnlimitedApp/Paginas/QRCode/EscreverQrCode.js
+++ UnlimitedApp/Paginas/QRCode/EscreverQrCode.js
@@ -1,99 +1,94 @@
-import React, { useState, useEffect } from "react"
+import React, { useState, useEffect } from "react";
 import {
   Text,
   View,
-  StyleSheet,
-  Button,
-  FlatList,
   TouchableOpacity,
   Alert,
   SafeAreaView,
   Image,
   ScrollView,
   TextInput,
-} from "react-native"
+} from "react-native";
 import {
   doc,
-  getDoc,
   arrayUnion,
   updateDoc,
   collection,
   getFirestore,
   onSnapshot,
-  addDoc,
-} from "firebase/firestore"
-import { getAuth, onAuthStateChanged } from "firebase/auth"
-import styles from "./QrCodeStyle"
-import { list, listAll } from "@firebase/storage"
+} from "firebase/firestore";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import styles from "./QrCodeStyle";
+import { FontAwesome5 } from "@expo/vector-icons";
 
-let listaEventos = []
-let listaCodigos = []
+let listaEventos = [];
+let listaCodigos = [];
 
-const db = getFirestore()
-const colRef = collection(db, "Evento")
+const db = getFirestore();
+const colRef = collection(db, "Evento");
 
 onSnapshot(colRef, (snapshot) => {
-  let mounted = true
+  let mounted = true;
   if (mounted) {
-    listaEventos = []
-    listaCodigos = []
+    listaEventos = [];
+    listaCodigos = [];
     snapshot.docs.forEach((doc) => {
-      listaEventos.push({ ...doc.data(), id: doc.id })
-      listaCodigos.push(doc.data().codigoEvento)
-    })
+      listaEventos.push({ ...doc.data(), id: doc.id });
+      listaCodigos.push(doc.data().codigoEvento);
+    });
   }
-  return () => (mounted = false)
-})
+  return () => (mounted = false);
+});
 
 const EscreverQrCode = ({ navigation }) => {
-  const [codigo, setCodigo] = useState()
-  const [utilizador, setUtilizador] = useState("null")
-  const [evento, setEvento] = useState()
-  const [utilizadorUtils, setUtilizadorUtils] = useState("null")
-  const [user, setUser] = useState()
+  const [codigo, setCodigo] = useState();
+  const [utilizador, setUtilizador] = useState("null");
+  const [evento, setEvento] = useState();
+  const [utilizadorUtils, setUtilizadorUtils] = useState("null");
+  const [user, setUser] = useState();
 
-  let utilizadorRef = null
-  let utilizadorUtilsRef = null
+  let utilizadorRef = null;
+  let utilizadorUtilsRef = null;
   useEffect(() => {
     //verificar se tem login feito
-    let isMounted = true
+    let isMounted = true;
     if (isMounted) {
-      const auth = getAuth()
+      const auth = getAuth();
       onAuthStateChanged(auth, (user1) => {
         if (user1) {
-          utilizadorRef = doc(db, "Utilizador", user1.email)
-          utilizadorUtilsRef = doc(db, "UtilizadorUtils", user1.email)
-          setUser(user1)
+          utilizadorRef = doc(db, "Utilizador", user1.email);
+          utilizadorUtilsRef = doc(db, "UtilizadorUtils", user1.email);
+          setUser(user1);
           onSnapshot(utilizadorRef, { includeMetadataChanges: true }, (doc) => {
             if (doc.exists()) {
-              setUtilizador(doc.data())
+              setUtilizador(doc.data());
             } else {
             }
-          })
+          });
           onSnapshot(
             utilizadorUtilsRef,
             { includeMetadataChanges: true },
             (doc) => {
               if (doc.exists()) {
-                setUtilizadorUtils(doc.data())
+                setUtilizadorUtils(doc.data());
               } else {
               }
             }
-          )
+          );
         } else {
         }
-      })
+      });
     }
     return () => {
-      isMounted = false
-    }
-  }, [])
+      isMounted = false;
+    };
+  }, []);
 
   function getPontos(code) {
     let aux = listaEventos.filter((item) => {
-      return item.codigoEvento == code
-    })
-    return aux[0].pontosAtribuidos
+      return item.codigoEvento == code;
+    });
+    return aux[0].pontosAtribuidos;
   }
 
   function testarCodigo() {
@@ -103,65 +98,75 @@ const EscreverQrCode = ({ navigation }) => {
     ) {
       let aux = (
         parseInt(utilizador.pontos) + parseInt(getPontos(codigo))
-      ).toString()
-      Alert.alert("Adicionado")
-      utilizadorRef = doc(db, "Utilizador", user.email)
+      ).toString();
+      Alert.alert("Adicionado");
+      utilizadorRef = doc(db, "Utilizador", user.email);
       updateDoc(utilizadorRef, {
         pontos: aux,
-      })
-      utilizadorUtilsRef = doc(db, "UtilizadorUtils", user.email)
+      });
+      utilizadorUtilsRef = doc(db, "UtilizadorUtils", user.email);
       updateDoc(utilizadorUtilsRef, {
         codigosEventos: arrayUnion(codigo),
-      })
+      });
     } else {
-      Alert.alert("Já adicionaste!")
+      Alert.alert("Já adicionaste!");
     }
   }
+
+  const goBack = () => {
+    navigation.goBack();
+  };
+
+  const HeaderInclinado = () => {
+    return (
+      <View>
+        <View
+          style={{
+            backgroundColor: "#1A649F",
+            height: 150,
+            width: "120%",
+            left: -50,
+            top: -50,
+            transform: [{ skewY: "-15deg" }],
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        />
+        <Image
+          style={styles.imageLogo}
+          source={require("../Login/unlimitedLogo.png")}
+        />
+        <TouchableOpacity style={styles.goBackBtn} onPress={goBack}>
+          <FontAwesome5 name="arrow-left" style={styles.goBackIcon} />
+        </TouchableOpacity>
+      </View>
+    );
+  };
 
   return (
     <SafeAreaView style={styles.safeArea}>
       <ScrollView style={styles.scrollView} bounces={false}>
-        <View style={styles.headerContainer}>
-          <View style={styles.headerRetangulo}></View>
-          <View style={styles.headerLogoContainer}>
-            <Image
-              style={styles.headerLogo}
-              source={require("../Login/unlimitedLogo.png")}
-            />
-          </View>
+        <HeaderInclinado />
+        <View style={styles.codeInput}>
+          <TextInput
+            placeholderTextColor="#174162"
+            placeholder="Colocar Código"
+            autoCapitalize="none"
+            type="text"
+            onChangeText={(text) => setCodigo(text)}
+            value={codigo}
+            style={styles.input}
+          ></TextInput>
         </View>
-        <View style={styles.codeContainer}>
-          <View style={styles.codeInput}>
-            <TextInput
-              placeholderTextColor="#174162"
-              placeholder="Colocar Código"
-              autoCapitalize="none"
-              type="text"
-              onChangeText={(text) => setCodigo(text)}
-              value={codigo}
-              style={styles.input}
-            ></TextInput>
-          </View>
-        </View>
-        <View style={styles.btnBox}>
-          <TouchableOpacity
-            style={styles.btnSubmeter}
-            onPress={() => testarCodigo()}
-          >
-            <Text style={styles.btnSubmeterText}>Submeter Código</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={styles.btnSubmeter}
-            onPress={() => {
-              navigation.navigate("QrCode")
-            }}
-          >
-            <Text style={styles.btnSubmeterText}>Voltar</Text>
-          </TouchableOpacity>
-        </View>
+        <TouchableOpacity
+          style={styles.btnSubmeter}
+          onPress={() => testarCodigo()}
+        >
+          <Text style={styles.btnSubmeterText}>Submeter Código</Text>
+        </TouchableOpacity>
       </ScrollView>
     </SafeAreaView>
-  )
-}
+  );
+};
 
-export default EscreverQrCode
+export default EscreverQrCode;
